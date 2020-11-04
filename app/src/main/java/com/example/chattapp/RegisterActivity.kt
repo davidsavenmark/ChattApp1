37b5a1056981e3dccf65c1f288e8e3f8fe5f823e
axelsode <axel.soderberg1996@gmail.com>
2020-11-04 15:07:53 +0100

    Added styles.xml

--- app/src/main/java/com/example/chattapp/RegisterActivity.kt
+++ app/src/main/java/com/example/chattapp/RegisterActivity.kt
@@ -3,26 +3,24 @@ package com.example.chattapp
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.TextView
 import androidx.appcompat.widget.Toolbar
 
 class RegisterActivity : AppCompatActivity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
 
         val toolbar: Toolbar = findViewById(R.id.toolbar_register)
-        setSupportActionBar(toolbar)
+       setSupportActionBar(toolbar)
         supportActionBar!!.title = "Register"
         supportActionBar!!.setDisplayHomeAsUpEnabled(true)
         toolbar.setNavigationOnClickListener {
             val intent = Intent(this@RegisterActivity, WelcomeActivity::class.java)
             startActivity(intent)
             finish()
-
-            finish()
         }
-
 
     }
 }
