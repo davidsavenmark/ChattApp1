15ac1e4d77214299587ce8dbfb8c88019c4a2932
David <dsinemark@gmail.com>
2020-11-17 11:25:40 +0100

    Added user_search_item_layout.xml and class UserAdapter.kt

---
+++ app/src/main/java/com/example/chattapp/AdapterClasses/UserAdapter.kt
@@ -0,0 +1,66 @@
+package com.example.chattapp.AdapterClasses
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.chattapp.ModelClasses.Users
+import com.example.chattapp.R
+import com.example.chattapp.data.ChatUser
+import de.hdodenhof.circleimageview.CircleImageView
+
+
+class UserAdapter(mContext: Context,
+mUsers: List <Users>,
+isChatCheck: Boolean
+) : RecyclerView.Adapter<UserAdapter.ViewHolder?>()
+
+{
+    private val mContext:Context
+    private val mUsers: List<Users>
+    private val isChatCheck: Boolean
+
+    init {
+        this.mUsers = mUsers
+        this.mContext = mContext
+        this.isChatCheck = isChatCheck
+
+
+    }
+
+    override fun onCreateViewHolder(viewGroup: ViewGroup, viewType: Int): ViewHolder {
+        val view: View = LayoutInflater.from(mContext).inflate(R.layout.user_search_item_layout, viewGroup, false)
+        return UserAdapter.ViewHolder(view)
+    }
+    override fun getItemCount(): Int {
+        return mUsers.size
+
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+
+    }
+
+    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
+    {
+        var userNameTxt: TextView
+        var profileImageView: CircleImageView
+        var onlineImageView: CircleImageView
+        var offlineImageView: CircleImageView
+        var lastMessageTxt: TextView
+
+        init
+        {
+            userNameTxt = itemView.findViewById(R.id.username)
+            profileImageView = itemView.findViewById(R.id.profile_image)
+            onlineImageView = itemView.findViewById(R.id.image_online)
+            offlineImageView = itemView.findViewById(R.id.image_offline)
+            lastMessageTxt = itemView.findViewById(R.id.message_last)
+        }
+    }
+
+
+
+
+}
